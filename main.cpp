2f3c6a4a019a1cf0b8a47436676610707b716cf2
Emmanuel M <70123271+PsychedelicPasta@users.noreply.github.com>
2022-01-01 16:57:22 -0600

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+
+
+int main(){
+
+  cout << "Stuff coming soon!!!" << endl << "I'm thinking of having a linked list (double linked list too), trees, graphs, stacks, queues, heaps, and hashmaps."
+
+}
